0def9093ea8c161c8ce215c2272524a84d940a00
Czechounek <adam-czech@email.cz>
2024-11-28 20:10:02 +0100

    first commit

---
+++ auth.py
@@ -0,0 +1,65 @@
+import jwt
+import bcrypt
+import mysql.connector
+from flask import jsonify
+from datetime import datetime, timedelta
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+# Secret key for encoding the JWT
+SECRET_KEY = os.environ.get('SECRET_KEY')
+
+# DB
+db_config = {
+    'host': os.getenv('DB_HOST'),
+    'user': os.getenv('DB_USER'),
+    'password': os.getenv('DB_PASS'),
+    'database': os.getenv('DB_NAME')
+}
+
+def auth(email, password):
+    conn = mysql.connector.connect(**db_config)
+    cursor = conn.cursor()
+    sql = "SELECT id, password FROM users WHERE email = %s"
+    cursor.execute(sql, (email,))
+    result = cursor.fetchone()
+    cursor.close()
+    conn.close()
+
+    if result is None or not bcrypt.checkpw(password.encode('utf-8'), result[1].encode('utf-8')):
+        return jsonify({'status': 'error', 'message': 'User auth failed: wrong email or password'}), 401
+    else:
+        #JWT 
+        payload = {
+            'user_id': result[0]
+        }
+        token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
+        return jsonify({'status': 'success', 'token': token}), 200
+
+def register(email, password, name, phone_number=None):
+    conn = mysql.connector.connect(**db_config)
+    cursor = conn.cursor()
+    sql = "SELECT * FROM users WHERE email = %s"
+    cursor.execute(sql, (email,))
+    result = cursor.fetchone()
+    if result is not None:
+        cursor.close()
+        conn.close()
+        return jsonify({'status': 'error', 'message': 'User already exists'}), 400
+    else:
+        hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+        if phone_number:
+            sql = "INSERT INTO users (email, password, name, phone_number) VALUES (%s, %s, %s, %s)"
+            cursor.execute(sql, (email, hashed_password.decode('utf-8'), name, phone_number))
+        else:
+            sql = "INSERT INTO users (email, password, name) VALUES (%s, %s, %s)"
+            cursor.execute(sql, (email, hashed_password.decode('utf-8'), name))
+        conn.commit()
+        cursor.close()
+        conn.close()
+        return auth(email, password)
+
+
---
+++ main.py
@@ -0,0 +1,148 @@
+from flask import Flask, request, jsonify, redirect
+import os
+from datetime import datetime
+from dotenv import load_dotenv
+import mysql.connector
+import bcrypt
+import ssl
+import jwt
+from register import register
+from auth import auth
+
+
+load_dotenv()
+
+app = Flask(__name__)
+conn = None
+ 
+# DB
+db_config = {
+    'host': os.getenv('DB_HOST'),
+    'user': os.getenv('DB_USER'),
+    'password': os.getenv('DB_PASS'),
+    'database': os.getenv('DB_NAME')
+}
+@app.route('/auth', methods=['POST'])
+def auth_endpoint():
+    data = request.get_json()
+    email = data.get('email')
+    password = data.get('password')
+
+    if (email is None) or (password is None):
+        return jsonify({'status': 'error', 'message': 'Missing parameters'}), 400
+    else:
+        return auth(email, password)
+
+@app.route('/register', methods=['POST'])
+def register_endpoint():
+    data = request.get_json()
+    email = data.get('email')
+    password = data.get('password')
+    name = data.get('name')
+    phone_number = data.get('phone_number')
+
+    if (email is None) or (password is None) or (name is None):
+        return jsonify({'status': 'error', 'message': 'Missing parameters'}), 400
+    else:
+        return register(email, password, name, phone_number)
+        auth(email, password)
+"""@app.route('/validation', methods=['POST'])
+def handle_validation_post():
+    data = request.get_json()
+    email = data.get('email')
+    password = data.get('password')
+    key = data.get('api_key')
+
+    if not email or not password or not key:
+        return jsonify({'status': 'error', 'message': 'Missing parameters'}), 400
+
+    conn = mysql.connector.connect(**db_config)
+    cursor = conn.cursor()
+
+    sql = "SELECT ID, api_info FROM programs WHERE api_key = %s"
+    cursor.execute(sql, (key,))
+
+    app_result = cursor.fetchone()
+    cursor.close()
+
+    if app_result is None:
+        return jsonify({'status': 'error', 'message': 'API key not found'}), 401
+    else:
+        sql = "SELECT key, valid_to, users.active, license.active, acces, passwd FROM license RIGHT JOIN users ON license.user_ID = users.ID WHERE email = %s AND program = %s"
+        cursor = conn.cursor()
+        cursor.execute(sql, (email, app_result[0]))
+
+        result = cursor.fetchone()
+        cursor.close()
+
+        if result is None or not bcrypt.checkpw(password.encode('utf-8'), result[5].encode('utf-8')):
+            return jsonify({'status': 'error', 'message': 'User not found'}), 601
+        else:
+            if (result[1] is not None):
+                if(result[1] < datetime.now().date()):
+                    return jsonify({'status': 'error', 'message': 'License expired'}), 602
+            if(result[2] == 0):
+                return jsonify({'status': 'error', 'message': 'User not active'}), 603
+            if(result[3] == 0):
+                return jsonify({'status': 'error', 'message': 'License not active'}), 604
+
+            return jsonify({
+                    'status': 'success',
+                    'key': result[0],
+                    'expire': result[1].isoformat() if result[1] else None,
+                    'access': result[4],
+                    'static': app_result[1],
+                    'time': datetime.now().isoformat() 
+            }), 200"""
+
+"""@app.route('/grouplist', methods=['POST'])
+def handle_post_group():
+    data = request.json
+    key = data.get('api_key')
+
+    if not key:
+        return jsonify({'status': 'error', 'message': 'Missing parameters'}), 400
+
+    conn = mysql.connector.connect(**db_config)
+    cursor = conn.cursor()
+
+    sql = "SELECT ID, api_info FROM programs WHERE api_key = %s"
+    cursor.execute(sql, (key,))
+
+    app_result = cursor.fetchone()
+    cursor.close()
+
+    if app_result is None:
+        return jsonify({'status': 'error', 'message': 'API key not found'}), 401
+    else:
+        sql = "SELECT group FROM license WHERE program = %s"
+        cursor = conn.cursor()
+        cursor.execute(sql, (app_result[0],))
+
+        result = cursor.fetchall()
+        cursor.close()
+
+        if result is None:
+            return jsonify({'status': 'error', 'message': 'Group not found'}), 605
+        else:
+            return jsonify({
+                'header': {
+                    'status': 'success',
+                    'time': datetime.now.value,
+                },
+                'groups': result
+            }), 200"""
+
+@app.route('/info', methods=['GET'])
+def test():
+    return jsonify({'status': 'success',
+                    'time': datetime.now(),
+                    'version': os.getenv("VERSION")}
+                    ), 200
+
+"""@app.route('/beacons', methods=['GET'])
+def get_beacons():"""
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
+    os.system('cls' if os.name == 'nt' else 'clear')
